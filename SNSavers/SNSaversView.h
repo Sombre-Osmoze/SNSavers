cff7cf8457f94ad140e5dc7b8b9f3089484da97b
Marcus Florentin <fmarcus28@gmx.fr>
2018-03-20 04:01:04 +0100

    Initial Commit

---
+++ SNSavers/SNSaversView.h
@@ -0,0 +1,13 @@
+//
+//  SNSaversView.h
+//  SNSavers
+//
+//  Created by Marcus Florentin on 20/03/2018.
+//  Copyright © 2018 Marcus Florentin. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SNSaversView : ScreenSaverView
+
+@end
